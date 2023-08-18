e3ba494a0bcc283d2ebd81c2d4cf930c5e54ee61
HlalalaEddie <86592137+HlalalaEddie@users.noreply.github.com>
2023-08-18 12:26:58 +0200

    Update UI.py

--- UI.py
+++ UI.py
@@ -37,12 +37,12 @@ def main():
     # User input for features
     st.header('Feature Input')
     
-    location_Type={0:'Urban',1:'Rural'}
+    location_Type={0:'Urban',1: 'Rural'}
     def format_func(option):
         return location_Type[option]
     feature1 = st.selectbox("Choose your Location Type", options=list(location_Type.keys()), format_func=format_func)
 
-     cellphone_access={0:'No',1:'Yes'}
+     cellphone_access={0:'No',1: 'Yes'}
     def format_func(option):
         return cellphone_access[option]
     feature2 = st.selectbox("cellphone_access", options=list(cellphone_access.keys()), format_func=format_func)
@@ -51,13 +51,13 @@ def main():
     feature3 = st.number_input('age_of_respondent', value=0)
 
 
-    education_level={0:'Secondary education',1:'Vocational/Specialised training',2:'No formal education', 3:'Primary education',4: 'Other/Dont know/RTA',5:'Tertiary education'}
+    education_level={0:'Secondary education',1: 'Vocational/Specialised training',2: 'No formal education', 3: 'Primary education',4: 'Other/Dont know/RTA',5: 'Tertiary education'}
     def format_func(option):
         return education_level[option]
     feature4 = st.selectbox("Choose your education_level", options=list(education_level.keys()), format_func=format_func)
 
    
-     Job_type={0:'Formally employed Government', 1:'Formally employed Private',2:'Remittance Dependent', 3:'Self employed', 4:'Informally employed',5:'Farming and Fishing', 6:'Government Dependent',7:'Other Income',8:'Dont Know/Refuse to answer', 9:'No Income'}
+     Job_type={0:'Formally employed Government', 1: 'Formally employed Private',2: 'Remittance Dependent', 3: 'Self employed', 4: 'Informally employed',5: 'Farming and Fishing', 6: 'Government Dependent',7: 'Other Income',8: 'Dont Know/Refuse to answer', 9: 'No Income'}
     def format_func(option):
         return Job_type[option]
     feature5 = st.selectbox("Choose your Job_type", options=list(Job_type.keys()), format_func=format_func)
